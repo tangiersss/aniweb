71933bf0d7a95789a0f00697f16dfb7744d34a71
Viktor <zouron11@gmail.com>
2026-01-26 00:05:46 +0200

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'router/router.dart';
 import 'theme/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const AnimeTrackerApp());
 }
 
